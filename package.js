d857ea2a2eca442affeb83c3aab957106a7a035b
Phat Pham <phatpham9@gmail.com>
2016-10-06 00:58:28 +0700

    Update documentation url

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
     name: "phatpham9:theme-hd",
     summary: "Telescope Hacking Days theme package",
     version: "0.0.1",
-    documentation: 'https://github.com/hackingdays/telescope-theme-hd/README.md',
+    documentation: 'https://github.com/hackingdays/telescope-theme-hd/blob/master/README.md',
     git: "https://github.com/hackingdays/telescope-theme-hd.git"
 });
 
